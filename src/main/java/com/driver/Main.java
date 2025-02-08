5af497e8d3abbd5ac23aadfcfd3a684fb078144f
sanket3033 <sanketppund@gmail.com>
2025-02-08 19:53:55 +0530

    methods - overriding assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+
+        B b = new B();
+        A a = (A) b;
+        System.out.println(a.meth());
+        System.out.println(b.meth());
+    }
   
 }
